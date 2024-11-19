60b983d68f7985568ad315b49b672b4ef79a316d
infinull <119103652+theinfinull@users.noreply.github.com>
2024-11-19 18:29:22 +0530

    added spline 3d model in home page

--- components/Home.js
+++ components/Home.js
@@ -5,8 +5,17 @@ export default {
   template: `
     ${header.template}
     <div class="home">
-        <h1>Open Multiple URLs with a single click!</h1>
+      <!-- SPLINE CONTENT -->
+      <spline-viewer url="https://prod.spline.design/qS0vegZw36uSv7Da/scene.splinecode"></spline-viewer>
+      <!-- spline hider -->
+      <div class="spline-overlay"></div>
+      <div class="spline-hider0"></div>
+      <div class="spline-hider1">
+        <div class="direction">Scroll Down ↘</div>
+      </div>
+      <!-- SPLINE CONTENT ENDS -->
+      <!-- PAGE CONTENT -->
+      <h1>Open Multiple URLs with a single click!</h1>
     </div>
-    ${footer.template}
     `,
 };
